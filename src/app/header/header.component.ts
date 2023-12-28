d32dc1611cf33515f09aac3330b5323066468915
HimaUndi <himamadhuri.u@calibrage.in>
2023-12-28 11:08:28 +0530

    changes related to the Routings

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   ]
 })
 export class HeaderComponent {
+  activeRoute: string = ''; // Store the active route dynamically
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    // Subscribe to router events to detect route changes
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Get the current route with the leading slash
+        this.activeRoute = this.router.url; // Set the active route dynamically
+      }
+    });
+  }
 }
--- src/app/services/sidebar/sidebar.component.ts
+++ src/app/services/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
   ]
 })
 export class SidebarComponent {
+  activeRoute: string = ''; // Store the active route dynamically
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    // Subscribe to router events to detect route changes
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Get the current route with the leading slash
+        this.activeRoute = this.router.url; // Set the active route dynamically
+      }
+    });
+  }
 }
